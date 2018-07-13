47d868568b08edcc44f3897560f7059df31db6fe
WinUP <l.gholoi@outlook.com>
2018-07-13 16:42:19 -0400

    Change xml attr$char name to support MongoDB

--- src/parsers/parseRetsResponse.ts
+++ src/parsers/parseRetsResponse.ts
@@ -4,7 +4,10 @@ import { IRetsBody, RetsProcessingError, RetsReplyError } from '../models';
 import { findReplyCodeName } from '../tools/ReplyCode';
 import { decodeHexString } from '../tools/decodeHexString';
 
-const parser = new Xml2Js.Parser({ ...Xml2Js.defaults['0.2'], explicitArray: false });
+const XML_ATTRIBUTES = '_XmlAttributes';
+const XML_CONTENT = '_XmlContent';
+
+const parser = new Xml2Js.Parser({ ...Xml2Js.defaults['0.2'], explicitArray: false, attrkey: XML_ATTRIBUTES, charkey: XML_CONTENT });
 
 async function parseString(content: string): Promise<{ [key: string]: any }> {
     return new Promise((resolve, reject) => {
@@ -20,7 +23,7 @@ async function parseString(content: string): Promise<{ [key: string]: any }> {
 
 function readNodeContent(source: any): string | undefined {
     if (typeof source === 'string') { return source; }
-    return source._;
+    return source[XML_CONTENT];
 }
 
 export async function parseRetsResponse(source: string, recordXmlTagName?: string): Promise<IRetsBody> {
@@ -29,20 +32,20 @@ export async function parseRetsResponse(source: string, recordXmlTagName?: strin
     if (!document.RETS) { throw new RetsProcessingError(new TypeError('Unable to find RETS root element')); }
     const root: { [key: string]: any } = document.RETS;
     const result: IRetsBody = {
-        replyCode: +root.$.ReplyCode,
-        replyText: root.$.ReplyText,
-        statusMessage: findReplyCodeName(root.$.ReplyCode) || 'Unknown reply code',
+        replyCode: +root[XML_ATTRIBUTES].ReplyCode,
+        replyText: root[XML_ATTRIBUTES].ReplyText,
+        statusMessage: findReplyCodeName(root[XML_ATTRIBUTES].ReplyCode) || 'Unknown reply code',
         extra: {}
     };
     if (root['RETS-STATUS']) { // 额外状态
-        const extraStatus = root['RETS-STATUS'].$ || {};
+        const extraStatus = root['RETS-STATUS'][XML_ATTRIBUTES] || {};
         result.replyCode = +(extraStatus.replyCode || result.replyCode);
         result.replyText = extraStatus.ReplyText || result.replyText;
         result.statusMessage = findReplyCodeName(result.replyCode) || 'Unknown reply code';
     }
     if (root['COLUMNS']) { // COMPACT数据
         let dataDelimiter = '\t';
-        if (root['DELIMITER']) { dataDelimiter = decodeHexString(root['DELIMITER'].$.value); }
+        if (root['DELIMITER']) { dataDelimiter = decodeHexString(root['DELIMITER'][XML_ATTRIBUTES].value); }
         const columns = (root['COLUMNS'] as string).split(dataDelimiter);
         const element: string | string[] | undefined = root['DATA'];
         const rawData = (element ? (element instanceof Array ? element : [element]) : []).map(v => v.split(dataDelimiter));
@@ -52,15 +55,15 @@ export async function parseRetsResponse(source: string, recordXmlTagName?: strin
             return p;
         }, {} as { [key: string]: string }));
     }
-    Object.keys(root).filter(v => v !== '$' && v !== 'RETS-STATUS' && v !== 'COLUMNS' && v !== 'DELIMITER' && v !== 'DATA').forEach(key => {
+    Object.keys(root).filter(v => v !== XML_ATTRIBUTES && v !== 'RETS-STATUS' && v !== 'COLUMNS' && v !== 'DELIMITER' && v !== 'DATA').forEach(key => {
         if (key === 'MAXROWS') {
             result.extra.maxRowsExceeded = true;
         } else if (key === 'COUNT') {
-            result.extra.count = root[key].$.Records;
+            result.extra.count = root[key][XML_ATTRIBUTES].Records;
         } else if (recordXmlTagName && key === recordXmlTagName) {
             result.records = (result.records || []).concat(root[key]);
         } else if (key === 'RETS-RESPONSE') { // RETS回执
-            if (root[key].$ && (root[key].$.xmlns || '').includes('CREA')) { // CREA DDF
+            if (root[key][XML_ATTRIBUTES] && (root[key][XML_ATTRIBUTES].xmlns || '').includes('CREA')) { // CREA DDF
                 const responseContent = root[key];
                 if (responseContent['Pagination']) {
                     const pagination = responseContent['Pagination'];
@@ -72,7 +75,7 @@ export async function parseRetsResponse(source: string, recordXmlTagName?: strin
                         returned: +pagination.RecordsReturned
                     };
                 }
-                Object.keys(responseContent).filter(v => v !== 'Pagination' && v !== '$' && v !== '_').forEach(data =>
+                Object.keys(responseContent).filter(v => v !== 'Pagination' && v !== XML_ATTRIBUTES && v !== XML_CONTENT).forEach(data =>
                     result.records = (result.records || []).concat(responseContent[data])
                 );
             } else { // 其他情况
